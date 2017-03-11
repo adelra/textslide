a3b791e0ccceadd46b3cc92fb306dff5fef59f92
adel <rahimi.adel@gmail.com>
2017-03-11 08:59:05 +0330

    updated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request, send_from_directory
 from pptx import Presentation
 import os
-from textslide import summarize
+from config import summarize
 
 app = Flask(__name__)
 
